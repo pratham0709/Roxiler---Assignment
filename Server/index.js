a5729b7d10dd010e0ca0f06924e9bfa08c5519a4
pratham0709 <prathameshjadhav792@gmail.com>
2024-08-13 23:20:48 +0530

    orgin added

--- Server/index.js
+++ Server/index.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(function (req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    const allowedOrigins ="*";
+    res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins =["http://localhost:3000","*","https://roxiler-assignment-two.vercel.app"]
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
       res.setHeader('Access-Control-Allow-Origin', origin);
