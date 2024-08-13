83942815a7ce8659dda3ce890b350758430af4ef
pratham0709 <prathameshjadhav792@gmail.com>
2024-08-13 23:07:25 +0530

    * corn

--- Server/index.js
+++ Server/index.js
@@ -12,10 +12,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 const corsOptions = {
-    origin: ['http://localhost:3000', 'https://roxiler-assignment-two.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Adjust methods as per your requirements
-    allowedHeaders: ['Content-Type', 'Authorization'], // Add any headers you expect
-    credentials: true, // Enable this if you need to include cookies or authorization headers
+    origin: "*",
+
+    credentials: false, // Enable this if you need to include cookies or authorization headers
 };
   
 app.use(bodyParser.json());
